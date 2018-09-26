6e1270549b69542264a962f88398640c6d53cdf1
Silvia Berella <silvia.berella@web.de>
2018-09-26 19:23:04 +0200

    Challenges-week-4-silviaberella-part1
    Challenges-week-4-silviaberella-part1

---
+++ js/channels.js
@@ -0,0 +1,45 @@
+/* Definition Yummy Objjekt" */
+var yummy = {
+    name:"#Yummy", 
+    createdOn:"April 1, 2016", 
+    createdBy: "minus.plus.yummy",  
+    starred: "false",
+    expriresIn: 100,
+    messageCount: 999
+};
+
+var sevenContinents = {
+    name:"#SevenContinents", 
+    createdOn:"April 1, 2016", 
+    createdBy: "minus.plus.yummy",  
+    starred: "true",
+    expriresIn: 100,
+    messageCount: 999
+};
+
+var killerApp = {
+    name:"#KillerApp", 
+    createdOn:"April 1, 2016", 
+    createdBy: "minus.plus.yummy",  
+    starred: "false",
+    expriresIn: 100,
+    messageCount: 999
+};
+
+var firstPersonOnMars = {
+    name:"#FirstPersonOnMars", 
+    createdOn:"April 1, 2016", 
+    createdBy: "minus.plus.yummy",  
+    starred: "true",
+    expriresIn: 100,
+    messageCount: 999
+};
+
+var octoberfest = {
+    name:"#Octoberfest", 
+    createdOn:"April 1, 2016", 
+    createdBy: "minus.plus.yummy",  
+    starred: "false",
+    expriresIn: 100,
+    messageCount: 999
+};
--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+/* Definition of global vaiables */
+var currentChannel = {
+    name:"#SevenContinents", 
+    createdOn:"April 1, 2016", 
+    createdBy: "minus.plus.yummy",  
+    starred: "true",
+    expriresIn: 100,
+    messageCount: 999
+};
+
+/* Definition der aktuellen Position */
+var currentLocation = {longitude: 47.793358399999995,latitude: -4.286696};
+console.log("Current Location", currentLocation);
+
 /* #6 start the #external #action and say hello */
 console.log("App is alive");
 
@@ -6,27 +20,81 @@ console.log("App is alive");
  * @param channelName Text which is set
  */
 function switchChannel(channelName) {
+  
     //Log the channel switch
     console.log("Tuning in to channel", channelName);
+    // Store current selected channel in global variable currentChannel
+    currentChannel = channelName;
+    console.log("Current Channel", currentChannel);
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = channelName.name;
 
     //#6 change the #channel #location
     document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    (channelName.starred == "true") ? ($('#channel-star').removeClass('far fa-star')) : ($('#channel-star').removeClass('fas fa-star'));
+    (channelName.starred == "true") ? ($('#channel-star').addClass('fas fa-star')) : ($('#channel-star').addClass('far fa-star'));
+ 
+    /*$('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');*/
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    $('#channels li:contains(' + channelName.name + ')').addClass('selected');
 }
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+    //currentChannel.starred = NOT(currentChannel.starred);
+    //console.log("In star function");
+    console.log("Current Channel", currentChannel);
+    //console.log("Current Channel Starred", currentChannel.starred);
+    /* $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png'); */
+    /* If clicking on star in app bar the star should toggle to filled star as before the image with filled star above has been displayed */
+    (currentChannel.starred == "true") ? ($('#channel-star').removeClass('fas fa-star')) : ($('#channel-star').removeClass('far fa-star'));
+    (currentChannel.starred == "true") ? ($('#channel-star').addClass('far fa-star')) : ($('#channel-star').addClass('fas fa-star'));
+       
+    // $('#channel-star').removeClass('far fa-star');
+    // $('#channel-star').addClass('fas fa-star');
+    (currentChannel.starred == "true") ? (currentChannel.starred = "false") : (currentChannel.starred = "true");
+    //currentChannel.starred = "true";
+             
+    console.log("Current Channel Starred", currentChannel.starred);
+    //currentChannel.name="#SevenContinents";
+    console.log("Current Channel.name", currentChannel.name);
+    //(currentChannel.starred == "true") ? ($('#star' + currentChannel.name).removeClass('far fa-star')) : ($('#star' + currentChannel.name).removeClass('fas fa-star'));
+        
+    /*
+    if (currentChannel.name.localeCompare("#Yummy")){
+       console.log("Ich bin im Yummy Channel");
+       (currentChannel.starred == "true") ? ($('#starYummy').removeClass('far fa-star')) : ($('#starYummy').removeClass('fas fa-star'));
+       (currentChannel.starred == "true") ? ($('#starYummy').addClass('fas fa-star')) : ($('#starYummy').addClass('far fa-star'));
+      }  else if (currentChannel.name.localeCompare("#SevenContintents")){
+        console.log("In if statement in #SevenContintents channel");
+        (currentChannel.starred == "true") ? ($('#starSevenContintents').removeClass('far fa-star')) : ($('#starSevenContintents').removeClass('fas fa-star'));
+        (currentChannel.starred == "true") ? ($('#starSevenContintents').addClass('fas fa-star')) : ($('#starSevenContintents').addClass('far fa-star'));
+      }  else if (currentChannel.name == "#KillerApp"){
+        (currentChannel.starred == "true") ? ($('#starKillerApp').removeClass('far fa-star')) : ($('#starKillerApp').removeClass('fas fa-star'));
+        (currentChannel.starred == "true") ? ($('#starKillerApp').addClass('fas fa-star')) : ($('#starKillerApp').addClass('far fa-star'));
+      }  else if (currentChannel.name == "#FirstPersonOnMars"){
+        (currentChannel.starred == "true") ? ($('#starFirstPersonOnMars').removeClass('far fa-star')) : ($('#starFirstPersonOnMars').removeClass('fas fa-star'));
+        (currentChannel.starred == "true") ? ($('#starFirstPersonOnMars').addClass('fas fa-star')) : ($('#starFirstPersonOnMars').addClass('far fa-star'));
+      } else if (currentChannel.name == "#Octoberfest"){
+        (currentChannel.starred == "true") ? ($('#starOctoberfest').removeClass('far fa-star')) : ($('#starOctoberfest').removeClass('fas fa-star'));
+        (currentChannel.starred == "true") ? ($('#starOctoberfest').addClass('fas fa-star')) : ($('#starOctoberfest').addClass('far fa-star'));
+      }
+      else {
+
+      }
+
+      */
+
+    //(currentChannel.starred == "true") ? ($('#star' + currentChannel.name).addClass('fas fa-star')) : ($('#star' + currentChannel.name).addClass('far fa-star'));
+    //(currentChannel.starred == "true") ? ($('#starSevenContintents').addClass('fas fa-star')) : ($('#starSevenContintents').addClass('far fa-star'));
+    console.log("Current Channel", currentChannel);
+    
 }
 
 /**
